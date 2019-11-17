456638ce63c34f051549c2c652cf50232b0ce23d
Carlos <carloslago@opendeusto.es>
2019-11-17 19:13:46 +0100

    Menu and tags

--- game_collection/views_tables.py
+++ game_collection/views_tables.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 
-from game_collection.models import Finished, Abandoned, Played, Playing, Queue, Interested, Wishlist
+from game_collection.models import Finished, Abandoned, Played, Playing, Queue, Interested, Wishlist, Tag
 
 
 def queue_table(request):
     titles = Queue.objects.filter(user=request.user)
+    custom = Tag.objects.filter(user=request.user)
 
     context = {
+        "tags": custom,
         "titles": titles
     }
 
@@ -15,8 +17,10 @@ def queue_table(request):
 
 def playing_table(request):
     titles = Playing.objects.filter(user=request.user)
+    custom = Tag.objects.filter(user=request.user)
 
     context = {
+        "tags": custom,
         "titles": titles
     }
 
@@ -25,8 +29,10 @@ def playing_table(request):
 
 def finished_table(request):
     titles = Finished.objects.filter(user=request.user)
+    custom = Tag.objects.filter(user=request.user)
 
     context = {
+        "tags": custom,
         "titles": titles
     }
 
@@ -35,8 +41,10 @@ def finished_table(request):
 
 def played_table(request):
     titles = Played.objects.filter(user=request.user)
+    custom = Tag.objects.filter(user=request.user)
 
     context = {
+        "tags": custom,
         "titles": titles
     }
 
@@ -45,8 +53,10 @@ def played_table(request):
 
 def abandoned_table(request):
     titles = Abandoned.objects.filter(user=request.user)
+    custom = Tag.objects.filter(user=request.user)
 
     context = {
+        "tags": custom,
         "titles": titles
     }
 
@@ -55,8 +65,10 @@ def abandoned_table(request):
 
 def insterested_table(request):
     titles = Interested.objects.filter(user=request.user)
+    custom = Tag.objects.filter(user=request.user)
 
     context = {
+        "tags": custom,
         "list_type": "interested",
         "titles": titles
     }
@@ -66,10 +78,25 @@ def insterested_table(request):
 
 def wishlist_table(request):
     titles = Wishlist.objects.filter(user=request.user)
+    custom = Tag.objects.filter(user=request.user)
 
     context = {
+        "tags": custom,
         "list_type": "wishlist",
         "titles": titles
     }
 
     return render(request, "collection/tables/list_table.html", context)
+
+
+def tag_table(request):
+    custom = Tag.objects.filter(user=request.user)
+    tag = Tag.objects.get(name=request.GET['tag'], user=request.user)
+    games = tag.game_version.all()
+    context = {
+        "tags": custom,
+        "list_type": tag.name,
+        "titles": games
+    }
+
+    return render(request, "collection/tables/tag_table.html", context)
